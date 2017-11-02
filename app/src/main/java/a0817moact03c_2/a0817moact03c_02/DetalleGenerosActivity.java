5d51e1002f0b3f4c63c7da777f1131da5651e243
Unknown <rodrigo-gangone@hotmail.com.ar>
2017-11-02 15:10:58 -0300

    Agregue mas peliculas
    .

--- app/src/main/java/a0817moact03c_2/a0817moact03c_02/DetalleGenerosActivity.java
+++ app/src/main/java/a0817moact03c_2/a0817moact03c_02/DetalleGenerosActivity.java
@@ -45,6 +45,12 @@ public class DetalleGenerosActivity extends AppCompatActivity implements Adaptad
             listaDePelis.add(new Pelicula("El transportador 10", R.drawable.imagenpeliculas, "Accion", "Un pelado trabado, que anda siempre arriba de un Audi enfierrado."));
             listaDePelis.add(new Pelicula("Indiana jones", R.drawable.imagenpeliculas, "Aventura", "Un exploraGay que siempre lleva su latigo encima."));
             listaDePelis.add(new Pelicula("Martes 13", R.drawable.imagenpeliculas, "Terror", "Un feo con una mascara que murio ahogado en un lago , resucita todos los martes 13 para matar a todos."));
+            listaDePelis.add(new Pelicula("Masacre de texas", R.drawable.imagenpeliculas,"Terror","Un carnisero loco con muy pocas pulgas mata a todo habitante nuevo en Texas"));
+            listaDePelis.add(new Pelicula("Anabelle",R.drawable.imagenpeliculas,"Terror","Una muñeca con un espiritu maligno se adueña de una familia, sus intenciones no son buenas"));
+            listaDePelis.add(new Pelicula("Freddy Cruger",R.drawable.imagenpeliculas,"Terror","Una persona acusada de algo que no hizo fue quemada viva por sus vecinos, vuelve en tus sueños en busca de venganza "));
+            listaDePelis.add(new Pelicula("IT", R.drawable.imagenpeliculas,"Terror","Un payaso loco en busca de niños para alimentarse vuelve cada 27 años a su suidad donde murio"));
+            listaDePelis.add(new Pelicula("SAW",R.drawable.imagenpeliculas,"Terror", "Un viejo loco asesina a toda persona culpable de pecados"));
+
 
 
     }
