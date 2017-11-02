60d0de30f4b68752230fa656bad2c9c44d8e3491
Seba Dominikow <sebidomi@hotmail.com>
2017-11-02 17:32:17 -0300

    Terminado : detalle de peliculas pasado a Fragment

--- app/src/main/java/a0817moact03c_2/a0817moact03c_02/DetallePeliculaActivity.java
+++ app/src/main/java/a0817moact03c_2/a0817moact03c_02/DetallePeliculaActivity.java
@@ -2,6 +2,8 @@ package a0817moact03c_2.a0817moact03c_02;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -12,7 +14,7 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.List;
 
-public class DetallePeliculaActivity extends AppCompatActivity implements AdaptadorDePeliculaRecycler.EscuchadorDePeliculas {
+public class DetallePeliculaActivity extends AppCompatActivity implements DetallePeliculaFragment.EscuchadorDePeliculasInterface {
     private List<Pelicula> listaDePeliculasSugeridas;
     private AdaptadorDePeliculaRecycler unAdaptadorDePeliculaRecycler;
 
@@ -23,75 +25,44 @@ public class DetallePeliculaActivity extends AppCompatActivity implements Adapta
 
         Intent unIntent = getIntent();
         Bundle unBundle = unIntent.getExtras();
-        String nombrePelicula = unBundle.get("nombre_pelicula").toString();
-        final int imagenDePelicula = (int) unBundle.get("Imagen_Pelicula");
-        String generoDePelicula = unBundle.get("Genero_Pelicula").toString();
-        String descripcionDePelicula = unBundle.getString("descripcion_pelicula").toString();
-        TextView textViewNombrePelicula = (TextView)findViewById(R.id.textViewDelTituloDeLaPelicula);
-        ImageView unImageViewPelicula = (ImageView)findViewById(R.id.imageViewDeLaPelicula);
-        TextView unTextViewDelGenero = (TextView)findViewById(R.id.textViewDelGenero);
-        TextView unTextViewDeLaDescripcion= (TextView) findViewById(R.id.textViewDeLaDescripcionDeLaPelicula);
 
 
-        textViewNombrePelicula.setText(nombrePelicula);
-        unImageViewPelicula.setImageResource(imagenDePelicula);
-        unTextViewDelGenero.setText(generoDePelicula);
-        unTextViewDeLaDescripcion.setText(descripcionDePelicula);
-
-        unImageViewPelicula.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent unIntent = new Intent(DetallePeliculaActivity.this,FotoCompletaPeliculaActivity.class);
-
-                Bundle unBundle = new Bundle();
-                unBundle.putInt("foto_completa",imagenDePelicula);
-                unIntent.putExtras(unBundle);
-                startActivity(unIntent);
-            }
-        });
-
-        //1) Buscar el recycler View
-        RecyclerView recyclerViewDePeliculasSugeridas = (RecyclerView)findViewById(R.id.recyclerViewDePeliculasSugeridas);
-
-        cargarPersonajesFavoritos();
-
-        //2) Crear el adaptador
-         unAdaptadorDePeliculaRecycler = new AdaptadorDePeliculaRecycler(listaDePeliculasSugeridas, this,this);
-
-        //Como quiero que muestre las celdas
-        recyclerViewDePeliculasSugeridas.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
-       // recyclerViewPersonaje.setLayoutManager(new GridLayoutManager(this,2));
-
-
-        //3) Setearle el adaptador al recycler view
-        //listViewPersonajes.setAdapter(unAdaptadorDePersonaje);
-        recyclerViewDePeliculasSugeridas.setAdapter(unAdaptadorDePeliculaRecycler);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        DetallePeliculaFragment detallePeliculaFragment = new DetallePeliculaFragment();
+        detallePeliculaFragment.setArguments(unBundle);
+        fragmentTransaction.replace(R.id.contenedorFragmentDetallePelicula,detallePeliculaFragment);
+        fragmentTransaction.commit();
 
     }
 
 
 
-    private void cargarPersonajesFavoritos(){
-        listaDePeliculasSugeridas = new ArrayList<>();
-        listaDePeliculasSugeridas.add(new Pelicula("MasacreDeTexas", R.drawable.imagenpeliculas,"terror","Un pipe medio chiflado sale a matar gente en el estado de TeJas."));
-        listaDePeliculasSugeridas.add(new Pelicula("Anabelle", R.drawable.imagenpeliculas,"terror", " Una muñeca de feria seenoja por no ser linda como la Barbie y mata a todos."));
-        listaDePeliculasSugeridas.add(new Pelicula("Freddy Crugger ", R.drawable.imagenpeliculas,"terror","Un pelado quemado ,más achicharrado que una pasa de uva aparece en tus sueños para hacer contigo lo que se le plazca ."));
-        listaDePeliculasSugeridas.add(new Pelicula(" IT ", R.drawable.imagenpeliculas,"terror", "Un payaso aparece cada 27 años en la matanza para robarse todos los celus."));
-        listaDePeliculasSugeridas.add(new Pelicula("Saw", R.drawable.imagenpeliculas,"terror", " Mueren todos."));
+
+
+
+
+    @Override
+    public void seleccionaronPelicula(Pelicula unaPelicula) {
+        Intent unIntent = new Intent(DetallePeliculaActivity.this,DetallePeliculaActivity.class);
+        Bundle unBundle = new Bundle();
+        unBundle.putString("nombre_pelicula",unaPelicula.getNombre());
+        unBundle.putInt("Imagen_Pelicula", unaPelicula.getImagen());
+        unBundle.putString("Genero_Pelicula",unaPelicula.getGenero());
+        unBundle.putString("descripcion_pelicula",unaPelicula.getDescripcion());
+        unIntent.putExtras(unBundle);
+        startActivity(unIntent);
 
     }
 
     @Override
-    public void seleccionaronA(Pelicula unaPelicula) {
+    public void seleccionaronImagen(int unInt) {
+        Intent unIntent = new Intent(this, FotoCompletaPeliculaActivity.class);
 
-            Intent unIntent = new Intent(DetallePeliculaActivity.this,DetallePeliculaActivity.class);
-            Bundle unBundle = new Bundle();
-            unBundle.putString("nombre_pelicula",unaPelicula.getNombre());
-            unBundle.putInt("Imagen_Pelicula", unaPelicula.getImagen());
-            unBundle.putString("Genero_Pelicula",unaPelicula.getGenero());
-            unBundle.putString("descripcion_pelicula",unaPelicula.getDescripcion());
-            unIntent.putExtras(unBundle);
-            startActivity(unIntent);
-        }
+        Bundle unBundle = new Bundle();
+        unBundle.putInt("foto_completa", unInt);
+        unIntent.putExtras(unBundle);
+        startActivity(unIntent);
 
+    }
 }
